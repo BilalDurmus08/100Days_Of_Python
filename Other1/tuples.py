af7368b03791e85d54378df1b2198b5c93ac48a3
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-07-13 17:01:57 +0300

    tuples class added

---
+++ Other1/tuples.py
@@ -0,0 +1,6 @@
+#Tuples are less flexible than list. Because that we can not sort, or modify them
+#The good thing is they are more efficient, performant
+my_tuple = (2, 3, 4, 5, 6)
+print(5 in my_tuple)
+#they are immutable like string and int
+
