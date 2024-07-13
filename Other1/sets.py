8f51e9162f43ed35835878dc43370183874a67e9
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-07-13 17:01:49 +0300

    sets class added

---
+++ Other1/sets.py
@@ -0,0 +1,14 @@
+#The sets are contain only unique items
+my_sets = {5, 7, 8, 11, 55, 55}
+
+print(my_sets)  #it won't print same item two times
+#in sets there is np o index like dictionary
+
+
+#Truth and Falsy
+something = "apple" #for boolean TRUE
+something1 = ""     #for boolean FALSE
+something2 = 0      #for boolean FALSE
+something3 = 20     #for boolean TRUE
+
+print(f"{bool(something)}\n{bool(something1)}\n{bool(something2)}\n{bool(something3)}")
